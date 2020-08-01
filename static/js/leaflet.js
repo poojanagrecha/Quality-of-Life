17a5a2d096203a001d21535e224fcf3b66fd3160
06S197GT <59593635+06S197GT@users.noreply.github.com>
2020-08-01 11:47:22 -0500

    Create branch, adding leaflet files

---
+++ static/js/leaflet.js
@@ -0,0 +1,29 @@
+var queryUrl = "data/ScoreInfo.csv";
+
+d3.csv(queryUrl, function(data) {
+    // Once we get a response, define variables
+    console.log(data);
+});
+var myMap = L.map("mapid", {
+    center: [40.7128, -74.0059],
+    zoom: 11
+});
+
+// Adding tile layer
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+}).addTo(myMap);
+
+// Use this link to get the geojson data.
+var link = "static/data/nyc.geojson";
+
+// Grabbing our GeoJSON data..
+d3.json(queryUrl, function(data) {
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJson(data).addTo(myMap);
+});
