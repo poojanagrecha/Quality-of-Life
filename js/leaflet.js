337d1e72a2b32c076ecfbb9e21edae86a38641ca
06S197GT <59593635+06S197GT@users.noreply.github.com>
2020-08-06 19:33:22 -0500

    Updated CSV and added final leaflet map

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+    let dropFilter = $("select").val();
+    createMap(dropFilter);
+
+    $("select").on("change", function() {
+        let dropFilter = this.value;
+        createMap(dropFilter);
+    });
+});
+
+function createMap(dropFilter) {
+    $('#mapParent').empty();
+    $('#mapParent').append('<div style="height:825px" id="mapid"></div>');
 
     var streetSatmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -23,31 +35,37 @@ $(document).ready(function() {
         accessToken: API_KEY
     });
 
-    var geoData = "data/convertScores.geojson";
+
+    var geoData = "data/Allinclusive.geojson";
 
     d3.json(geoData).then(function(cityData) {
-        console.log(cityData.features[0].geometry.coordinates);
 
         var markers = L.markerClusterGroup();
+
         var circles = [];
 
         var city = cityData.features;
+
         city.forEach(function(place) {
+
             if ((place.geometry.coordinates[1]) && (place.geometry.coordinates[0])) {
-                console.log(place.properties);
-                let popUp = L.marker([+place.geometry.coordinates[1], +place.geometry.coordinates[0]]).bindPopup("<h3>" + place.properties.City + "<h3> ");
+
+                let popUp = L.marker([+place.geometry.coordinates[1], +place.geometry.coordinates[0]]).bindPopup("<h3>" + place.properties.Address + "</h3><hr><h5>" + dropFilter.split('_').join(' ') + " Score: " + Math.round(place.properties[dropFilter], 2) + "</h5>");
                 markers.addLayer(popUp);
 
                 let circle = L.circle([+place.geometry.coordinates[1], +place.geometry.coordinates[0]], {
-                    radius: 5,
-                    fillColor: "black",
+                    radius: markerSize(place.properties[dropFilter]),
+                    fillColor: circleColor(place.properties[dropFilter]),
                     fillOpacity: .9,
                     color: 'grey',
                     weight: .5
-                }).bindPopup("<h3>" + place.properties.City + "<h3> ");
+                }).bindPopup("<h3>" + place.properties.Address + "</h3><hr><h5>" + dropFilter.split('_').join(' ') + " Score: " + Math.round(place.properties[dropFilter], 2) + "</h5>");
 
                 circles.push(circle);
+
             }
+
+
         });
 
         var circleLayer = L.layerGroup(circles);
@@ -60,31 +78,35 @@ $(document).ready(function() {
 
         // Create overlay object to hold our overlay layer
         var overlayMaps = {
-            "Earthquake Clusters": markers,
-            "Earthquake Circles": circleLayer,
+            "City Markers": markers,
+            "City Circles": circleLayer,
         };
 
         var myMap = L.map("mapid", {
             center: [39.833333, -98.583333],
             zoom: 4.7,
-            layers: [streetSatmap, markers]
+            layers: [darkmap, circleLayer]
         });
 
         var legend = L.control({ position: 'bottomright' });
         legend.onAdd = function(myMap) {
 
             var div = L.DomUtil.create('div', 'legend'),
-                magnitudes = [0, 1, 2, 3, 4, 5]
+                scores = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-            div.innerHTML += "<h4>Magnitude</h4>"
-            for (var i = 0; i < magnitudes.length; i++) {
+            div.innerHTML += "<h4>Scores</h4>"
+            for (var i = 0; i < scores.length; i++) {
                 div.innerHTML +=
-                    '<i style="background:' + circleColor(magnitudes[i] + 1) + '"></i> ' +
-                    magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+                    '<i style="background:' + circleColor(scores[i] + 1) + '"></i> ' +
+                    scores[i] + (scores[i + 1] ? '&ndash;' + scores[i + 1] + '<br>' : '+');
             }
             return div;
 
 
+        };
+
+        function markerSize(dot) {
+            return dot * 10000;
         };
         legend.addTo(myMap);
 
@@ -97,11 +119,19 @@ $(document).ready(function() {
             collapsed: false
         }).addTo(myMap);
     })
-});
+};
 
 function circleColor(x) {
-    var colorFill = ['#82b74b', '#b5e7a0', '#f2e394', '#f2ae72', '#d96459', '#c83349'];
-    if (x > 5) {
+    var colorFill = ['#ff0066', '#e60f75', '#bf268c', '#993da3', '#804cb2', '#665cc2', '#4073d9', '#1a8af0', '#0099ff', '#00CCFF'];
+    if (x > 9) {
+        return colorFill[9];
+    } else if (x > 8) {
+        return colorFill[8];
+    } else if (x > 7) {
+        return colorFill[7];
+    } else if (x > 6) {
+        return colorFill[6];
+    } else if (x > 5) {
         return colorFill[5];
     } else if (x > 4) {
         return colorFill[4];
