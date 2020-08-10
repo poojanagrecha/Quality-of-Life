9fa74c4077712639f9714e6ef7de5c3a5e74497f
Daniel C <59593635+06S197GT@users.noreply.github.com>
2020-08-10 15:54:27 -0500

    Cleaned up files

--- js/leaflet.js
+++ js/leaflet.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
 function createMap(dropFilter) {
     $('#mapParent').empty();
-    $('#mapParent').append('<div style="height:827px" id="mapid"></div>');
+    $('#mapParent').append('<div style="height:760px" id="mapid"></div>');
 
     var streetSatmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
