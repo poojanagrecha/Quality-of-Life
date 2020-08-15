e638a18c7c7ee303c1b071cc920128a22f52aa32
Daniel C <59593635+06S197GT@users.noreply.github.com>
2020-08-15 09:36:14 -0500

    Update bar.js

--- js/bar.js
+++ js/bar.js
@@ -132,8 +132,8 @@ function buildCircleBarplot1(activeIndicator) {
 
         circleBars.on('mouseover', function(data) {
                 toolTip.show(data, this);
-                toolTip.style("top", (height + innerRadius - innerRadius / 5) + "px")
-                    .style("left", (width - (width / 1.8)) + "px")
+                toolTip.style("top", (height + innerRadius - innerRadius) + "px")
+                    .style("left", (width - (width / 1.84)) + "px")
                 d3.select(this)
                     .transition()
                     .duration(1000)
